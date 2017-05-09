fd13ff7aa3753d6fc50fd140474bb37319768e31
Jvros <crono1985@gmail.com>
2017-05-09 17:28:05 +0200

    Initial config server test

---
+++ config-service/src/main/java/jvros/test/cloud/services/config/ConfigServer.java
@@ -0,0 +1,16 @@
+package jvros.test.cloud.services.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by JTRO on 09/05/2017.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServer.class, args);
+    }
+}
