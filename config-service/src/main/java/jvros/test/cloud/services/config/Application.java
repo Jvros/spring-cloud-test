a6d94739865d1a0a65c756d5dfb20ae9d0366fa1
Jvros <crono1985@gmail.com>
2017-05-10 13:54:24 +0200

    Config files moved server

--- config-service/src/main/java/jvros/test/cloud/services/config/Application.java
+++ config-service/src/main/java/jvros/test/cloud/services/config/Application.java
@@ -9,8 +9,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServer {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServer.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
--- discovery-service/src/main/java/jvros/test/cloud/services/discovery/Application.java
+++ discovery-service/src/main/java/jvros/test/cloud/services/discovery/Application.java
@@ -9,10 +9,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServer {
+public class Application {
 
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(DiscoveryServer.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
